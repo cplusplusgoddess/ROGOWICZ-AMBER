096b03686bcfeaeb567c93d6f83e59bc913ba8c5
cplusplusgoddess <amberwaves@nethere.com>
2022-06-09 01:04:31 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,74 +1,38 @@
-// ############################################
-// Author: 	Amber Rogowicz
-// File	:	main.cpp   test main for running NeoMatrix 
-// Date:	July 2018
-
-
 
 #include <iostream>
-#include <string>
+#include <chrono>
 #include "neomatrix.hpp"
 
-using namespace std;
 
-using namespace neolib;
 
-// if matrix_test receives ANY arguments, it tests for help and 
-// or attempts to parse the first command line parameter into an
-// integer for use as the number of threads, otherwise runs with a
-// default of 4
-// matrix_test runs single threaded without arguments
+using namespace matlib;
 
-const int MAX_THREADS = 4;   // default number of threads 
+// if main receives ANY arguments, its assumed the user wants to 
+// run matrix multiplication with threading 
 
-static void show_usage(std::string name)
-{
-    std::cerr << "Usage: " << name << "  | -h | # | [ANY GARBAGE]\n"
-              << "\t-h,--help\t\tShow this help message\n"
-              << "\t WITH NO PARAMETERS \t\t means run single threaded \n"
-              << "\t # \t\t\t = run with the Maximum NUMBER_OF_THREADS > 1\n"
-              << "\t [ANY GARBAGE]\t\t  = run with the default NUMBER_OF_THREADS = 4\n"
-              << std::endl;
-}
+// const int MAX_THREADS = 4;   // hardcoding for simplicity
+
 int main(int argc, char *argv[]) 
 {
     if (argc > 1) 
 	{
-    	int num_threads = MAX_THREADS;
-    	std::string arg  = argv[1];
-        if ((arg == "-h") || (arg == "--help")) 
-		{
-            // user  is asking for help to run the program
-            show_usage(argv[0]);
-            return(1);
-    	}
-		try {
-            // string -> integer
-            num_threads = std::stoi(arg);
-		} catch(...){
-        		// Number of threads in command line arguments is not parsable
-    			num_threads = MAX_THREADS;
-            	//show_usage(argv[0]);
-            	//return(1);
-    	} 
-        neolib::initMatLib(num_threads);
-	  	std::cout << "Running multi-threaded with "<< num_threads << " threads\n\n";
+	  size_t MAX_THREADS = std::thread::hardware_concurrency();
+
+	  size_t user_num_threads = atoi(argv[1]);
+	  std::cout << "The number of available processing cores: " << MAX_THREADS << std::endl;
+      matlib::initMatLib(user_num_threads<=MAX_THREADS?user_num_threads:MAX_THREADS);
+	  std::cout << "Running multi-threaded...\n\n";
     } else 
 	  std::cout << "Running single threaded...\n\n";
 
 
-    Matrix simpleA={ { 1.0, 1.0, 1.0 } , 
-					 { 1.0, 1.0, 1.0 } , 
-					 { 1.0, 1.0, 1.0 } , 
-					 { 1.0, 1.0, 1.0 } , 
-					 { 1.0, 1.0, 1.0 } } ;
-    // Matrix simpleA(5, 3);
-    // simpleA = 1.0;
+    Matrix simpleA(8, 3);
     Matrix simpleB(3, 2);
 	Matrix A(108, 2041);
-	Matrix B(2041, 1041), C(1041, 2043), D(2043, 41), E(41, 3601); 
-	Matrix F(3601, 5701), G(5701, 206), H(206, 1041), I(1041, 6);
+	Matrix B(2041, 1041), C(1041, 3), D(3, 41), E(41, 3601); 
+	Matrix F(3601, 5701), G(5701, 6), H(6, 1041), I(1041, 5);
 	
+    simpleA = 1.0;
     simpleB = 2.0;
 	A = 1.00;
 	B = 0.05;
@@ -76,30 +40,43 @@ int main(int argc, char *argv[])
 	D = -0.25;
 	E = 0.005;
 	F = 0.0025;
-	G = -0.20;
-	H = 0.00075;
+	G = -0.50;
+	H = 2.00;
 	I = 1.00;
 	
 
-	// print the intended operation and results
-	//std::cout << A << "   * \n" << B << "   * \n" << C  << "   * \n" << D  << "   * \n" << E  << "   * \n"
-    //				<< F  << "   * \n"<< G  << "   * \n" << H  << "   * \n" << I  << "   = \n" ;
-	std::cout << "A * B * C * D * E * F * G * H * I = \n";
-	Matrix aiResult = A * B * C * D * E * F * G * H * I;
-	std::cout << aiResult << "\n\n";
-	std::cout << "~  aiResult =\n\n " ;
-	
-	std::cout << aiResult.transpose() << "\n\n" ;
 
-	std::cout << simpleA << "\n\t*\n\n" << simpleB << "\n\t= \n" ;
+	std::cout << "Multiplying simple matricies: \n";
+    std::cout << "simpleA: \n" << simpleA << std::endl;
+    std::cout << "simpleB: \n" << simpleB << std::endl;
 	Matrix simple = simpleA * simpleB ;
-	std::cout << simple;
+	std::cout << "dot product result: \n" << simple << std::endl;
 
+
+	std::cout << "Transpose of Dot product of matricies: [" << H.getNumRows() << "][" << H.getNumCols() << "] * ["<< I.getNumRows() << "][ " << I.getNumCols() << "]\nWhere A = 2.0, B = 1.0\n\n" ;   
+
+	Matrix abResult = H * I ;
+	std::cout << abResult.transpose();
+	
+	std::cout << "Dot product of matricies: [" << A.getNumRows() << "][" << A.getNumCols() << "] * ["<< B.getNumRows() << "][ " << B.getNumCols() << "] * [" << C.getNumRows() << "][" << C.getNumCols() << "] \n" ;   
+	std::cout << "Where A = 1.0, B = 0.5, C = 1.0 \n\n\n";
+	std::cout << A * B * C << "\n\n\n";
+	A.initialize_random();
+	B.initialize_random();
+	C.initialize_random();
+	D.initialize_random();
+	E.initialize_random();
+	F.initialize_random();
+	G.initialize_random();
 	H.initialize_random();
 	I.initialize_random();
-	//std::cout << H << "\n\t*\n" << I << "\n\t=\n" ;
-	Matrix abResult = H * I ;
-	std::cout << abResult;
-	
+	std::cout << "Matricies: A * B * C * D * E * F * G * H * I initialized randomly [-1.0 -> 1.0]\n\n";
+    auto start_time = std::chrono::high_resolution_clock::now();
+	Matrix aiResult = A * B * C * D * E * F * G * H * I;
+	auto processing_time = std::chrono::high_resolution_clock::now() - start_time;
+	std::cout << aiResult << std::endl;
+	// std::cout << aiResult.transpose();
+	std::cout << "\nProcessing time: " << processing_time.count() << std::endl;
+
 	return 0;
 }
